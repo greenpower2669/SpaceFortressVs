ec0d1ceb2334b196bceb045db036f52857ed16f1
greenpower2669 <80179295+greenpower2669@users.noreply.github.com>
2026-09-05 20:37:02 +0200

    Add SDL Android activity

---
+++ android/app/src/main/java/com/greenpower2669/spacefortressvs/SpaceFortressActivity.java
@@ -0,0 +1,25 @@
+package com.greenpower2669.spacefortressvs;
+
+import org.libsdl.app.SDLActivity;
+
+public class SpaceFortressActivity extends SDLActivity {
+    @Override
+    protected String[] getLibraries() {
+        return new String[] {
+            "SDL2",
+            "SDL2_image",
+            "SDL2_mixer",
+            "main"
+        };
+    }
+
+    @Override
+    protected String getMainSharedObject() {
+        return getApplicationInfo().nativeLibraryDir + "/libmain.so";
+    }
+
+    @Override
+    protected String getMainFunction() {
+        return "SDL_main";
+    }
+}
